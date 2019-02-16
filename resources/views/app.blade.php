ad496e713e539c80c2b593400bf9961e0f032595
Mikolaj Marciniak <mikoo1991@yahoo.com>
2019-02-16 11:54:26 +0100

    small update

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -2,6 +2,9 @@
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     <head>
         <meta charset="utf-8">
+        <meta name="description" content="Online chat rooms for everyone! Chat online for free!">
+        <meta name="robots" content="index, follow">
+        <meta name="keywords" content="chat, online chat, livechats, live chats, live chat, chat rooms"
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
         <title>LiveChats</title>
