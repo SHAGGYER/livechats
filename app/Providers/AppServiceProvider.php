027947cf063ea8a93bbafbe4b5a88e638a750653
Mikolaj Marciniak <mikoo1991@yahoo.com>
2020-06-18 21:04:04 +0200

    update

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -25,9 +25,5 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         Schema::defaultStringLength(191);
-        if (!$this->app->isLocal()) {
-            URL::forceScheme('https');
-
-        }
     }
 }
